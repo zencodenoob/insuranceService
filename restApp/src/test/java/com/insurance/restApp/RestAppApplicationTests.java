9a33916ad23e8bed751c770fa26bb219bc22bf01
Davis Johnson <daviskoluvally@gmail.com>
2023-05-21 16:52:33 +0530

    spring dependencies added

---
+++ restApp/src/test/java/com/insurance/restApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.restApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
